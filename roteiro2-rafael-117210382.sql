192d384c266099ec6ee699edb699a360c437ac8d
Rafaeldsa <rafael.azevedo@ccc.ufcg.edu.br>
2019-03-29 21:43:39 -0300

    ok

--- roteiro2-rafael-117210382.sql
+++ roteiro2-rafael-117210382.sql
@@ -102,10 +102,53 @@ INSERT INTO funcionario(cpf,data_nasc,nome,funcao,nivel,superior_cpf) VALUES ('1
 --INSERT INTO funcionario(cpf,data_nasc,nome,funcao,nivel,superior_cpf) VALUES ('12345678913', '1980-04-09','joao da Silva', 'LIMPEZA', 'J', null);
 --não deve funcionar
 
+--Questão 9
 INSERT INTO funcionario(cpf,data_nasc,nome,funcao,nivel,superior_cpf) VALUES ('12345678913', '1972-04-15','Alvaro Dantas', 'SUP_LIMPEZA', 'P', null);
 INSERT INTO funcionario(cpf,data_nasc,nome,funcao,nivel,superior_cpf) VALUES ('12345678914', '1972-03-25','Gabriel Dantas', 'LIMPEZA', 'J', '12345678913');
 INSERT INTO funcionario(cpf,data_nasc,nome,funcao,nivel,superior_cpf) VALUES ('12345678915', '1972-03-27','Marilane Azevedo', 'SUP_LIMPEZA', 'S', null);
 INSERT INTO funcionario(cpf,data_nasc,nome,funcao,nivel,superior_cpf) VALUES ('12345678916', '1972-01-08','Rafaela Dantas', 'LIMPEZA', 'P', '12345678915');
+INSERT INTO funcionario(cpf,data_nasc,nome,funcao,nivel,superior_cpf) VALUES ('12345678917', '1972-01-01','Clara Miranda', 'LIMPEZA', 'J', '12345678916');
+INSERT INTO funcionario(cpf,data_nasc,nome,funcao,nivel,superior_cpf) VALUES ('12345678918', '1972-01-31','Aline Miranda', 'LIMPEZA', 'P', '12345678917');
+INSERT INTO funcionario(cpf,data_nasc,nome,funcao,nivel,superior_cpf) VALUES ('12345678919', '1972-01-10','Alic Miranda', 'SUP_LIMPEZA', 'S', null);
+INSERT INTO funcionario(cpf,data_nasc,nome,funcao,nivel,superior_cpf) VALUES ('12345678920', '1972-01-08','Helena Miranda', 'SUP_LIMPEZA', 'P', null);
+INSERT INTO funcionario(cpf,data_nasc,nome,funcao,nivel,superior_cpf) VALUES ('12345678921', '1972-01-08','Marli Miranda', 'LIMPEZA', 'S', '12345678920');
+INSERT INTO funcionario(cpf,data_nasc,nome,funcao,nivel,superior_cpf) VALUES ('12345678922', '1972-01-08','Julia Vitoria', 'SUP_LIMPEZA', 'P', null);
 
+--INSERT INTO funcionario(cpf,data_nasc,nome,funcao,nivel,superior_cpf) VALUES ('12345678913', '1972-04-15','Juninho', 'SUP_LIMPEZA', 'P', null); ERROR:  duplicate key value violates unique constraint "funcionario_pkey"
+--DETAIL:  Key (cpf)=(12345678913) already exists.
 
+--INSERT INTO funcionario(cpf,data_nasc,nome,funcao,nivel,superior_cpf) VALUES ('12345678923', '1972-03-25','Gabriel Dantas', 'SUP_LIMPEZA', 'J', '12345678913'); ERROR:  new row for relation "funcionario" violates check constraint "checksuperior"
+--DETAIL:  Failing row contains (12345678923, 1972-03-25, Gabriel Dantas, SUP_LIMPEZA, J, 12345678913).
 
+--INSERT INTO funcionario(cpf,data_nasc,nome,funcao,nivel,superior_cpf) VALUES ('123456789154', '1972-03-27','Marilane Azevedo', 'SUP_LIMPEZA', 'S', null); ERROR:  value too long for type character(11)
+
+--INSERT INTO funcionario(cpf,data_nasc,nome,funcao,nivel,superior_cpf) VALUES ('12345678916', '1972-01-08','Rafaela Dantas', 'LIMPEZA', 'P', null);ERROR:  new row for relation "funcionario" violates check constraint "checksuperior"
+--DETAIL:  Failing row contains (12345678916, 1972-01-08, Rafaela Dantas, LIMPEZA, P, null).
+
+--INSERT INTO funcionario(cpf,data_nasc,nome,funcao,nivel,superior_cpf) VALUES ('12345678925', '1972-01-01',null, 'SUP_LIMPEZA', 'J', null); ERROR:  null value in column "nome" violates not-null constraint
+--DETAIL:  Failing row contains (12345678925, 1972-01-01, null, SUP_LIMPEZA, J, null).
+
+--INSERT INTO funcionario(cpf,data_nasc,nome,funcao,nivel,superior_cpf) VALUES ('12345678945', '1972-01-314','Aline Miranda', 'LIMPEZA', 'P', '12345678917');ERROR:  date/time field value out of range: "1972-01-314"
+--LINE 1: ...funcao,nivel,superior_cpf) VALUES ('12345678945', '1972-01-3...
+
+--INSERT INTO funcionario(cpf,data_nasc,nome,funcao,nivel,superior_cpf) VALUES ('12345678932', '1972-01-10','Alic Miranda', 'SUP_LIMPEZA', 'S', '13245'); ERROR:  new row for relation "funcionario" violates check constraint "checksuperior"
+--DETAIL:  Failing row contains (12345678932, 1972-01-10, Alic Miranda, SUP_LIMPEZA, S, 13245      ).
+
+--INSERT INTO funcionario(cpf,data_nasc,nome,funcao,nivel,superior_cpf) VALUES (null, '1972-01-08','Helena Miranda', 'SUP_LIMPEZA', 'P', null); ERROR:  null value in column "cpf" violates not-null constraint
+--DETAIL:  Failing row contains (null, 1972-01-08, Helena Miranda, SUP_LIMPEZA, P, null).
+
+--INSERT INTO funcionario(cpf,data_nasc,nome,funcao,nivel,superior_cpf) VALUES ('12345678921', '1972-01-08','Marli Miranda', 'LIMPEZA', 'S', '12345678920'); ERROR:  duplicate key value violates unique constraint "funcionario_pkey"
+--DETAIL:  Key (cpf)=(12345678921) already exists.
+
+--INSERT INTO funcionario(cpf,data_nasc,nome,funcao,nivel,superior_cpf) VALUES ('12345678938', '1972-01-08','Julia Vitoria', 'SUP_LIMPEZA', 'L', null); ERROR:  new row for relation "funcionario" violates check constraint "checknivel"
+--DETAIL:  Failing row contains (12345678938, 1972-01-08, Julia Vitoria, SUP_LIMPEZA, L, null).
+
+--Questão 10
+INSERT INTO tarefas(id,descricao,func_resp_cpf,prioridade,status) VALUES ('2147483653','Limpeza do quarto gamer','12345678913',5,'C');
+
+INSERT INTO funcionario(cpf,data_nasc,nome,funcao,nivel,superior_cpf) VALUES ('32323232911', '1972-01-08','Anão', 'SUP_LIMPEZA', 'P', null);
+INSERT INTO funcionario(cpf,data_nasc,nome,funcao,nivel,superior_cpf) VALUES ('32323232955', '1972-01-08','Ana', 'SUP_LIMPEZA', 'P', null);
+INSERT INTO funcionario(cpf,data_nasc,nome,funcao,nivel,superior_cpf) VALUES ('98765432122', '1972-01-08','josé junior', 'LIMPEZA', 'P', '32323232911');
+INSERT INTO funcionario(cpf,data_nasc,nome,funcao,nivel,superior_cpf) VALUES ('98765432111', '1972-03-30','Victor', 'LIMPEZA', 'J', '32323232911');
+
+ALTER TABLE tarefas ADD CONSTRAINT fgkey_cpf FOREIGN KEY (func_resp_cpf) REFERENCES funcionario (cpf) ON DELETE CASCADE;
